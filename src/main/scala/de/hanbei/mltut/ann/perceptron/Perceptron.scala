9f07efb3b68ce4ecdb27edf05e04635efa216e65
Florian Schulz <florian.schulz@hanbei.de>
2013-10-12 18:18:04 +0200

    fixed training bug

--- src/main/scala/de/hanbei/mltut/ann/perceptron/Perceptron.scala
+++ src/main/scala/de/hanbei/mltut/ann/perceptron/Perceptron.scala
@@ -7,23 +7,26 @@ import de.hanbei.mltut.ann.Neuron
 class Perceptron(inputSize : Int) extends Neuron(x => if (x >= 0.5) 1 else 0, inputSize + 1) {
   var learningRate : Double = 0.1
 
-
   override def compute(input : Seq[Double]) : Double = super.compute(List(1.0) ++ input)
 
-  def train(data : Seq[Seq[Double]], labels : Seq[Int]) = {
+  def train(data : Seq[Seq[Double]], labels : Seq[Int]) : Unit = {
     for (i <- 1 to 1000) {
+      var errorCount = 0
       for ((dataPoint, label) <- data.zip(labels)) {
 
         val adjustedData = List(1.0) ++ dataPoint
 
-        val currentOutput = compute(adjustedData)
+        val currentOutput = compute(dataPoint)
         val error = label - currentOutput
 
         if (error != 0) {
-          var i = 0
+          errorCount += 1
           val tmp_weights = weights.zipWithIndex.map(x => x._1 + learningRate * error * adjustedData(x._2))
           weights = tmp_weights
         }
+      }
+      if (errorCount == 0) {
+        return
       }
     }
   }
